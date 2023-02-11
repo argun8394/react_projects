bf26f396a3064dee82fb13beb5092c525b2b8ddf
argun8394 <argundoganali@gmail.com>
2023-02-11 17:02:24 +0300

    initial

---
+++ instagram-react-web-clone/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <div>
+        <img src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" alt=""/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
