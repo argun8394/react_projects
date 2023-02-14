109bee9c7876ff9eae8daf2cdcde1666d2323b16
argun8394 <argundoganali@gmail.com>
2023-02-14 13:36:32 +0300

    image slider

--- instagram-react-web-clone/src/App.js
+++ instagram-react-web-clone/src/App.js
@@ -1,9 +1,48 @@
+import { useEffect, useRef } from "react";
+
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    let images = ref.current.querySelectorAll('img'),
+      total = images.length,
+      current = 0
+
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add('opacity-0')
+      } else {
+        images[total - 1].classList.add('opacity-')
+      }
+
+      images[current].classList.remove('opacity-0')
+      if (current === total - 1) {
+        current = 0
+      } else {
+        current += 1
+      }
+    }
+
+    imageSlider()
+    let interval = setInterval(imageSlider, 3000)
+    return () => {
+      clearInterval()
+    }
+    
+
+    },[ref])
   return (
     <div className="h-full w-full flex items-center justify-center">
-      <div>
-        <img src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" alt=""/>
+      <div className="w-[380px] h-[581px] bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px] relative" >
+        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-500 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-500 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-500 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-500 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png" alt="" />
+
+        </div>
+
       </div>
     </div>
   );
